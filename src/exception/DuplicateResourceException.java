c6e485b5ce8bcc2041d7f0f8ce16afc53f522d4d
Ablaykhan <dauka20015@gmail.com>
2026-01-23 09:47:32 +0500

    git init git add .gitignore README.md git commit -m "init: project structure and base files"

--- src/exception/DuplicateResourceException.java
+++ src/exception/DuplicateResourceException.java
@@ -1,4 +1,12 @@
 package exception;
 
-public class DuplicateResourceException {
+public class DuplicateResourceException extends InvalidInputException {
+
+    public DuplicateResourceException(String message) {
+        super(message);
+    }
+
+    public DuplicateResourceException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
