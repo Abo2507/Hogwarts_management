555e58c46e11a65f9702e5388b21f0ecaa3bd4ab
Ablaykhan <dauka20015@gmail.com>
2026-01-23 09:22:32 +0500

    I wrote interface package/

--- src/interfaces/Scoreable.java
+++ src/interfaces/Scoreable.java
@@ -1,25 +1,8 @@
 package interfaces;
-
-/**
- * Interface for entities that can have points
- * Demonstrates: Interface design and polymorphism
- */
 public interface Scoreable {
-    /**
-     * Adds points to the entity
-     * @param points number of points to add
-     */
-    void addPoints(int points);
+     void addPoints(int points);
 
-    /**
-     * Deducts points from the entity
-     * @param points number of points to deduct
-     */
     void deductPoints(int points);
 
-    /**
-     * Gets current points
-     * @return current point total
-     */
     int getPoints();
 }
--- src/interfaces/Validatable.java
+++ src/interfaces/Validatable.java
@@ -2,14 +2,7 @@ package interfaces;
 
 import exception.InvalidInputException;
 
-/**
- * Interface for entities that can be validated
- * Demonstrates: Interface design and polymorphism
- */
 public interface Validatable {
-    /**
-     * Validates the entity's data
-     * @throws InvalidInputException if validation fails
-     */
+
     void validate() throws InvalidInputException;
 }
